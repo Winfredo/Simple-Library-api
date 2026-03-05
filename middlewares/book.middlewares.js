bc13408afe0f9f91dc1b433ea5cd7e27584d2e89
winfredo <winfrednukpe2002@gmail.com>
2026-03-05 00:38:43 +0000

    book middlewares

---
+++ middlewares/book.middlewares.js
@@ -0,0 +1,46 @@
+import User from "../models/User.js";
+
+ const checkIfLoggedIn = async (req, res, next) => {
+  try {
+    const userId = req.headers["x-user-id"];
+
+    if (!userId) {
+      return res.status(401).json({
+        success: false,
+        message: "No user ID provided. Please log in.",
+      });
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found. Please log in again.",
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Authentication error",
+      error: error.message,
+    });
+  }
+};
+
+ const checkIfLibrarian = (req, res, next) => {    
+    if (req.user.role !== 'librarian') {
+        return res.status(403).json({ 
+            success: false, 
+            message: 'Access denied. Only librarians can perform this action.' 
+        })
+    }
+    
+    next()
+}
+export { checkIfLoggedIn, checkIfLibrarian }
+
