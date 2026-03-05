9133519e08f823d22dbdf223a7afdf74054d19ff
winfredo <winfrednukpe2002@gmail.com>
2026-03-05 22:18:33 +0000

    fixes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router();
-import {validateSignup, validateLogin} from '../middlewares/index.js'
+import {validateSignup, validateLogin, validateObjectId} from '../middlewares/index.js'
 import {userSignup, userLogin,userDelete} from '../controllers/index.js'
 
 router.post('/register', validateSignup, userSignup)
 router.post('/login', validateLogin, userLogin)
-router.delete('/:id', userDelete)
+router.delete('/:id', validateObjectId('id'), userDelete)
 
 export default router;
