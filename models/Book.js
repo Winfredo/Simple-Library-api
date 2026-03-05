777d4ec6edd0c92616c0e6107c30a7efbd2b308d
winfredo <winfrednukpe2002@gmail.com>
2026-03-05 00:38:53 +0000

    book models

--- models/Book.js
+++ models/Book.js
@@ -4,6 +4,7 @@ const bookSchema = new mongoose.Schema({
     title: {type: String, required: true},
     author: {type: String, required: true},
     genre: {type: String, required: true},
+    publishedYear: {type: Number, required: true}
 })
 
 const Book = mongoose.model('Book', bookSchema)
