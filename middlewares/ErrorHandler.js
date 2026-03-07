0ce999f9d366a407452472cf806048dfb5cb9578
winfredo <winfrednukpe2002@gmail.com>
2026-03-07 23:43:49 +0000

    completing the page.

--- middlewares/ErrorHandler.js
+++ middlewares/ErrorHandler.js
@@ -1,6 +1,14 @@
 
-const ErrorHandler = () => {
+const ErrorHandler = (err, req, res, next) => {
+    const errStatus = err.status || 500;
+    const errMessage = err.message || 'Internal Server Error';
 
+    res.status(errStatus).json({
+        success: false,
+        message: errMessage,
+        status: errStatus,
+        stack: process.env.NODE_ENV === 'production' && err.stack
+    })
 }
 
 export default ErrorHandler;
