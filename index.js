3ad139c7d700bbd6ab186ef35ed69e24460cdcc5
winfredo <winfrednukpe2002@gmail.com>
2026-05-06 12:49:45 +0000

    install cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import connectDB from './config/db.js';
 import {AuthRoutes} from './routes/index.js';
 import {BookRoutes} from './routes/index.js';
@@ -10,6 +11,7 @@ const PORT = 3000;
 dotenv.config({ quiet: true })
 
 app.use(express.json());
+app.use(cors());
 
 await connectDB();
 
