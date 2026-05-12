943d977e147560c9fc2b152883ad2a35cfcb6515
winfredo <winfrednukpe2002@gmail.com>
2026-05-12 23:02:46 +0000

    adding status of the book to the schema.

--- models/Book.js
+++ models/Book.js
@@ -4,7 +4,8 @@ const bookSchema = new mongoose.Schema({
     title: {type: String, required: true},
     author: {type: String, required: true},
     genre: {type: String, required: true},
-    publishedYear: {type: Number, required: true}
+    publishedYear: {type: Number, required: true},
+    status: {type: String, enum: ['available', 'borrowed'], default: 'available'}
 })
 
 const Book = mongoose.model('Book', bookSchema)
