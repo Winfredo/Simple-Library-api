778b53de22204ec1c6c1a44de86759c4e0fa444c
winfredo <winfrednukpe2002@gmail.com>
2026-05-12 23:09:44 +0000

    Borrow book schema.

---
+++ models/Borrow.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const BorrowSchema = new mongoose.Schema({
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    book: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+        required: true
+    },
+    borrowedAt: {
+        type: Date,
+        default: Date.now
+    },
+    dueDate: {
+        type: Date,
+        required: true
+    },
+    returnedAt: {
+        type: Date,
+        default: null
+    },
+    status: {
+        type: String,
+        enum: ['active', 'returned', 'overdue'],
+        default: 'active'
+    }
+})
+
+const Borrow = mongoose.model('Borrow', BorrowSchema);
+export default Borrow;
