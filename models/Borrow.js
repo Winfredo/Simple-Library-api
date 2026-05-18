716e648d8fceb83c9584dcf13a7e6451af59ad3d
winfredo <winfrednukpe2002@gmail.com>
2026-05-18 15:39:41 +0000

    add borrowId virtual to Borrow schema

--- models/Borrow.js
+++ models/Borrow.js
@@ -1,6 +1,42 @@
+// import mongoose from "mongoose";
+
+// const BorrowSchema = new mongoose.Schema({
+//     student: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User',
+//         required: true
+//     },
+//     book: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Book',
+//         required: true
+//     },
+//     borrowedAt: {
+//         type: Date,
+//         default: Date.now
+//     },
+//     dueDate: {
+//         type: Date,
+//         required: true
+//     },
+//     returnedAt: {
+//         type: Date,
+//         default: null
+//     },
+//     status: {
+//         type: String,
+//         enum: ['active', 'returned', 'overdue'],
+//         default: 'active'
+//     }
+// })
+
+// const Borrow = mongoose.model('Borrow', BorrowSchema);
+// export default Borrow;
+
+
 import mongoose from "mongoose";
 
-const BorrowSchema = new mongoose.Schema({
+const borrowSchema = new mongoose.Schema({
     student: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
@@ -28,7 +64,14 @@ const BorrowSchema = new mongoose.Schema({
         enum: ['active', 'returned', 'overdue'],
         default: 'active'
     }
-})
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
 
-const Borrow = mongoose.model('Borrow', BorrowSchema);
+borrowSchema.virtual('borrowId').get(function() {
+    return this._id;
+});
+
+const Borrow = mongoose.model('Borrow', borrowSchema);
 export default Borrow;
