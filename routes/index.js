4149d84d1ce6ccebb958639bdd4b3eae7a76d9fd
winfredo <winfrednukpe2002@gmail.com>
2026-06-04 11:43:36 +0000

    export default route for user route.

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
 export {default as AuthRoutes} from './auth.routes.js'
 export {default as BookRoutes} from './book.routes.js'
 export {default as BorrowRoutes} from './borrow.routes.js'
+export {default as UserRoutes} from './user.routes.js'
